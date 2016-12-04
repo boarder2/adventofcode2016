ec7bb62139f0650d19f52889cbeb18eadcedb7fe
Willie Zutz <boarder2@bit-shift.com>
2016-12-04 00:46:00 -0700

    Day 3 complete.

--- src/day3/part1.rs
+++ src/day3/part1.rs
@@ -4,7 +4,22 @@ use utils;
 
 pub fn run() {
     let input = input::get_input();
-    let lines = input.split_whitespace();
-    
-    println!("Day 3 Part 1: ");
+    let lines = input.lines();
+    let mut count_good = 0;
+
+    for line in lines {
+        let str_numbers = line.split_whitespace();
+        let mut parsed_numbers = Vec::new();
+        for str_num in str_numbers {
+            let num = str_num.parse::<i32>().expect("couldn't parse");
+            parsed_numbers.push(num);
+        }
+        parsed_numbers.sort();
+        if parsed_numbers[0] + parsed_numbers[1] > parsed_numbers[2] {
+            count_good += 1;
+        }
+    }
+
+
+    println!("Day 3 Part 1: {}", count_good);
 }
--- src/day3/part2.rs
+++ src/day3/part2.rs
@@ -4,7 +4,35 @@ use utils;
 
 pub fn run() {
     let input = input::get_input();
-    let lines = input.split_whitespace();
-    
-    println!("Day 3 Part 2: ");
+    let lines = input.lines();
+    let mut number_of_lines = 0;
+    let mut parsed_numbers = Vec::new();
+    let mut count_good = 0;
+    //Convert all strings to numbers, left to right, top to bottom
+    for line in lines {
+        let str_numbers = line.split_whitespace();
+        for str_num in str_numbers {
+            let num = str_num.parse::<i32>().expect("couldn't parse");
+            parsed_numbers.push(num);
+        }
+        number_of_lines += 1;
+    }
+
+    for iter in 0..number_of_lines / 3 {
+        let start_line = iter * 3;
+        for i_column in 0..3 {
+            let mut current_nums = Vec::new();
+        
+            for i_line in 0..3 {
+                current_nums.push(parsed_numbers[((start_line + i_line) * 3) + i_column]);
+            }
+
+            current_nums.sort();
+            if current_nums[0] + current_nums[1] > current_nums[2] {
+                count_good += 1;
+            }
+        }
+    }
+
+    println!("Day 3 Part 2: {}", count_good);
 }
