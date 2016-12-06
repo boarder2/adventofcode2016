f674d076b0b6ae966e21cc80854f9264d524cca4
Willie Zutz <boarder2@bit-shift.com>
2016-12-06 01:48:46 -0700

    Day 5 Part 1 finished

---
+++ src/day5/part2.rs
@@ -0,0 +1,7 @@
+// use day5::input;
+
+pub fn run() {
+	// let input = input::get_input();
+
+	println!("Day 5 Part 2: ");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-mod day1;
-mod day2;
-mod day3;
-mod day4;
+// mod day1;
+// mod day2;
+// mod day3;
+// mod day4;
+mod day5;
 mod utils;
 extern crate regex;
+extern crate crypto;
 
 fn main() {
-	day1::part1::run();
-	day1::part2::run();
-	day2::part1::run();
-	day2::part2::run();
-	day3::part1::run();
-	day3::part2::run();
-	day4::part1::run();
-	day4::part2::run();
+	// day1::part1::run();
+	// day1::part2::run();
+	// day2::part1::run();
+	// day2::part2::run();
+	// day3::part1::run();
+	// day3::part2::run();
+	// day4::part1::run();
+	// day4::part2::run();
+	// day5::part1::run();
+	day5::part2::run();
 }
