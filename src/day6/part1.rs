9b298c4f6c9b7c66e2c0966b53f8be8fd27017e6
Willie Zutz <boarder2@bit-shift.com>
2016-12-07 00:16:00 -0700

    Day 6 Part 1 complete

--- src/day6/part1.rs
+++ src/day6/part1.rs
@@ -1,7 +1,27 @@
 use day6::input;
+use std::collections::HashMap;
 
 pub fn run() {
 	let input = input::get_input();
+	let mut columns = Vec::new();
+	let mut result = String::new();
 
-	println!("Day 6 Part 1: ");
+	for _ in 0..input.split_whitespace().next().unwrap().len() {
+		let map: HashMap<char, u32> = HashMap::new();
+		columns.push(map);
+	}
+
+	for line in input.split_whitespace() {
+		for (i, ch) in line.chars().enumerate() {
+			*columns[i].entry(ch).or_insert(0) += 1;
+		}
+	}
+
+	for column in columns {
+		let mut map_array: Vec<_> = column.iter().collect();
+		map_array.sort_by(|a, b| b.1.cmp(a.1));
+		result.push(*map_array[0].0);
+	}
+
+	println!("Day 6 Part 1: {}", result);
 }
