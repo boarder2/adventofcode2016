1d83a047a75aae047ef00c78b2d4318be7a19605
Willie Zutz <boarder2@gmail.com>
2016-12-10 16:44:12 -0700

    Day 8 Part 2 complete

--- src/day8/part2.rs
+++ src/day8/part2.rs
@@ -1,7 +1,3 @@
-use day8::input;
-
-pub fn run() {
-	let input = input::get_input();
-	
-	println!("Day 8 Part 2: Not implemented");
+pub fn run() {	
+	println!("Day 8 Part 2: Solved in output of Part 1.");
 }
