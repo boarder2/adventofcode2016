7e9299ef124becd8310b533445ce621a32ea00ab
Willie Zutz <boarder2@bit-shift.com>
2016-12-16 02:20:34 -0700

    Day 16 - twice as fast.

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,46 +1,33 @@
 pub fn run() {
 	let fill_length_part_1 = 272;
 	let fill_length_part_2 = 35651584;
-	let input = String::from("01110110101001000");
+	let input = String::from("01110110101001000").chars().map(|c| c == '1').collect::<Vec<bool>>();
 
-	let mut data = generate_data(input.to_string(), fill_length_part_1);
+	let mut data = generate_data(input.clone(), fill_length_part_1);
 	let mut checksum = generate_checksum(data);
-	println!("Day 16 Part 1: {}", checksum);
+	println!("Day 16 Part 1: {}", checksum.into_iter().map(|x| if x { '1' } else { '0' }).collect::<String>());
 
 	data = generate_data(input, fill_length_part_2);
 	checksum = generate_checksum(data);
-	println!("Day 16 Part 2: {}", checksum);
+	println!("Day 16 Part 2: {}", checksum.into_iter().map(|x| if x { '1' } else { '0' }).collect::<String>());
 }
 
-fn generate_data(s: String, fill_length: usize) -> String {
-	let mut data = s;
+fn generate_data(d: Vec<bool>, fill_length: usize) -> Vec<bool> {
+	let mut data = Vec::with_capacity(fill_length);
+	data.extend(d);
 	while data.len() < fill_length {
-		let mut new_data = String::with_capacity(data.len() + 1);
-		new_data.push_str(&data);
-		new_data.push('0');
-		for ch in data.chars().rev() {
-			if ch == '0' {
-				new_data.push('1');
-			} else {
-				new_data.push('0');
-			}
-		}
-		data = new_data;
+		let mut rev = data.iter().rev().map(|x| !x).collect::<Vec<bool>>();
+		data.push(false);
+		data.append(&mut rev);
 	}
-	data[0..fill_length].to_string()
+	data.truncate(fill_length);
+	data
 }
 
-fn generate_checksum(s: String) -> String {
-	let mut iter = s.chars();
-	let mut checksum = String::with_capacity(s.len() / 2);
-	for _ in 0..s.len() / 2 {
-		let ch1 = iter.next().unwrap();
-		let ch2 = iter.next().unwrap();
-		if ch1 == ch2 {
-			checksum.push('1');
-		} else {
-			checksum.push('0');
-		}
+fn generate_checksum(d: Vec<bool>) -> Vec<bool> {
+	let mut checksum = Vec::with_capacity(d.len() / 2);
+	for chunk in d.chunks(2) {
+		checksum.push(chunk[0] == chunk[1]);
 	}
 	if checksum.len() % 2 == 0 {
 		return generate_checksum(checksum);
