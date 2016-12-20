036e9b0201b97d1437042d7bd7ffb240f99177cd
Willie Zutz <boarder2@bit-shift.com>
2016-12-20 00:35:16 -0700

    Tidying up day 20

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,5 +1,6 @@
 use std::cmp;
 mod input;
+const RANGE_MAX: u32 = 4294967295;
 
 #[derive(Debug, Copy, Clone)]
 struct S {
@@ -8,33 +9,27 @@ struct S {
 }
 
 pub fn run() {
-	let input = input::get_input();
 	let mut first_entry: i64 = -1;
 	let mut count_allowed: u32 = 0;
-	let mut sorted = input.lines()
+	let mut sorted = input::get_input()
+		.lines()
 		.collect::<Vec<_>>()
 		.iter()
 		.map(|l| {
 			let mut a = l.split('-');
-			let min: u32 = a.next().unwrap().parse().unwrap();
-			let max: u32 = a.next().unwrap().parse().unwrap();
-			let s = S {
-				min: min,
-				max: max,
-			};
-			s
+			S {
+				min: a.next().unwrap().parse().unwrap(),
+				max: a.next().unwrap().parse().unwrap(),
+			}
 		})
 		.collect::<Vec<_>>();
 	sorted.sort_by_key(|x| x.min);
 
-	//Ideally use references so we're not copying but meh.
+	// Ideally use references so we're not copying but meh.
 	let mut largest_entry = sorted[0];
 	for entry in sorted {
-		if entry.max > largest_entry.max && entry.min < largest_entry.max {
+		if entry.max > largest_entry.max && entry.min <= largest_entry.max {
 			largest_entry = entry;
-		} else if (entry.min == largest_entry.min && entry.max == largest_entry.max) ||
-		          (entry.min < largest_entry.max && entry.max < largest_entry.max) {
-			continue;
 		} else if entry.min > largest_entry.max {
 			if first_entry < 0 {
 				first_entry = (largest_entry.max + 1) as i64;
@@ -43,7 +38,7 @@ pub fn run() {
 			largest_entry = entry;
 		}
 	}
-	count_allowed += cmp::max((4294967295 - cmp::min(largest_entry.max, 4294967295)), 1) - 1;
+	count_allowed += cmp::max((RANGE_MAX - cmp::min(largest_entry.max, RANGE_MAX)), 1) - 1;
 	println!("Day 20 Part 1: {:?}", first_entry);
 	println!("Day 20 Part 2: {:?}", count_allowed);
 }
